17082cedf02123b436ae5e1edd79275f77b3ae5c
SYPIAC <Reks888@hotmail.com>
2018-09-19 01:51:30 +0300

    Moved AI to generic_env and added it to enemy turn. Should be done now?

--- generic_env/envs/generic_env.py
+++ generic_env/envs/generic_env.py
@@ -2,8 +2,11 @@ import numpy as np
 import hex_helpers as hh
 import copy
 import gym
+import time
 from gym import spaces
 
+#TODO:stop hardcoding the player
+#TODO:generate boards randomly
 board = [
     [-1, -1, 1, 0, 0, 0, 0, -1, -1],
     [-1, -1, 0, 0, 0, 0, 0, -1, -1],
@@ -31,6 +34,7 @@ defaultState = {
     'current_move': 1
 }
 state = defaultState
+
 #AGENT IS ALWAYS PLAYER 1
 class GenericEnv(gym.Env):
     def __init__(self):
@@ -45,18 +49,24 @@ class GenericEnv(gym.Env):
         self.observation_space = state['board'] #is this right
         self.reward_range = [0, 25*25] #score is reward???
         self.metadata = self.metadata #???
+        
+        #opposition AI
+        self.enemyAI = MinimaxAgent(2, ply=1)
         return
-    def step(self, action):
+    def step(self, action, debug=0):
         global state
+        
         state = apply_move(action, state)
+        #Greedy AI processing
+        move = self.enemyAI.select_move(state, debug)
+        state = apply_move(move, state)
+        if debug:
+            print(is_done(state))
         reward = score(1, state)
-        #TODO:Do AI processing here
-        #ai.move
-        #this line replaces ai processing, delete it
-        state['current_move'] = 1
         done = is_done(state)
         self.action_space = moves(1, state)
         self.observation_space = state['board']
+
         return self.observation_space, reward, done, 0
     def reset(self):
         state = defaultState
@@ -67,7 +77,59 @@ class GenericEnv(gym.Env):
         return self.observation_space
     def render(self, mode='human', close=False):
         return
-    
+
+#TODO:Put in a different module???(can't right now because of circular dependency)
+class MinimaxAgent:
+    def __init__(self, player, ply=1):
+        self.player = player
+        self.ply = ply
+        self.states_evaluated = 0
+        self.actions_taken = 0
+
+    def select_move(self, state, debug=False):
+        self.states_evaluated = 0
+        t1 = time.time()
+        if debug:
+            print('Agent {} making move...'.format(self.player))
+        action = self.look_forward(state)
+        if debug:
+            print(action)
+            print('States evalueated - {}'.format(self.states_evaluated))
+            print('Time taken: {}'.format(time.time() - t1))
+        return action
+
+    def look_forward(self, state):
+        cstate = copy.deepcopy(state)
+        all_my_moves = moves(2, cstate)
+        actions = []
+
+        for m in all_my_moves:
+            self.states_evaluated += 1
+            new_state = apply_move(m, cstate)
+            scores = self.minimax(new_state, self.ply-1)
+            actions.append({ 'action': m, 'score': scores, 'immediate_score': score(self.player, new_state)})
+        best_action = max(actions, key=lambda p: (p['score'], p['immediate_score']))
+        return best_action['action']
+
+    def minimax(self, state, depth=-1):
+        player = self.player
+        if is_done(state) or depth == 0:
+            return score(player, state)
+        else:
+            actions = moves(2, state)
+            scores = []
+            for a in actions:
+                self.states_evaluated += 1
+                s = apply_move(a, state)
+                scores.append(self.minimax(s, depth=depth-1))
+
+            if player == self.player:
+                maximum = np.argmax(scores)
+                return scores[maximum]
+            else:
+                minimum = np.argmin(scores)
+                return scores[minimum]
+            
 def player(state):
     return state['current_move']
 
@@ -100,7 +162,6 @@ def apply_move(move, state):
     board = new_state['board']
     player = move['player']
     opponent = 1 if player == 2 else 2
-
     if new_state['current_move'] != player:
         raise ValueError('Not your move')
 
