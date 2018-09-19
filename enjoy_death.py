0ef1dafa90f007546caddaf206490e8e6781233c
SYPIAC <Reks888@hotmail.com>
2018-09-19 21:16:38 +0300

    Fixed game done status, added learner to main and display to enjoy_death.py

---
+++ enjoy_death.py
@@ -0,0 +1,16 @@
+import gym
+import generic_env
+import generic_env_helpers
+from random import randint
+from baselines import deepq
+
+env = gym.make('GenericEnv-v1')
+act = deepq.learn(env, network='mlp', total_timesteps=0, load_path="cartpole_model.pkl")
+
+obs, done = env.reset(), False
+episode_rew = 0
+while not done:
+    obs, rew, done, _ = env.step(act(obs)[0])
+    print("p1:", generic_env_helpers.score(1, env.state), "\t\t\tp2:", generic_env_helpers.score(2, env.state))
+print("Score:", rew)
+print("\nBoard\n",env.state['board'])
--- main.py
+++ main.py
@@ -9,28 +9,16 @@ def callback(lcl, _glb):
     return is_solved
 
 env = gym.make('GenericEnv-v1')
-# env = gym.make('CartPole-v0')
-# env = gym.make('FrozenLake-v0')
 act = deepq.learn(
     env,
     network='mlp',
     lr=1e-3,
     total_timesteps=100000,
     buffer_size=50000,
-    exploration_fraction=0.999999,
-    exploration_final_eps=0.002,
-    print_freq=500,
-    callback=callback,
-    batch_size=32,
-    learning_starts=100
+    exploration_fraction=0.1,
+    exploration_final_eps=0.02,
+    print_freq=10,
+    callback=callback
 )
-'''
-for _ in range(100):
-    action = randint(0, env.action_space.n - 1)
-    print("PLAYER 1 DOING:",action)
-    observation, reward, done, _ = env.step(action,debug=1)
-    print("CURRENT REWARD:",reward)
-    if done:
-        print("\n=====\nGAME IS OVER NOW\n=====\n")
-        break
-'''
+print("\n=====\nSaving model to generic_env_model.pkl\n=====\n")
+act.save("generic_env_model.pkl")
