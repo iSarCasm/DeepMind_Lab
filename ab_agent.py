4b4cbcd71314842f464a5f0c3d4c58ddd919c0a7
SarCasm <sarcasm008@gmail.com>
2018-09-22 10:01:59 +0300

    sdfsdf

--- ab_agent.py
+++ ab_agent.py
@@ -4,7 +4,7 @@ import time
 import numpy as np
 import pprint
 
-class MinimaxAgent:
+class AbAgent:
     def __init__(self, player, ply=1):
         self.player = player
         self.opponent = 1 if player == 2 else 2
@@ -20,41 +20,65 @@ class MinimaxAgent:
             print('Agent {} making move...'.format(self.player))
 
         cstate = copy.deepcopy(state)
-        action = self.alphabeta(cstate, self.ply, -1e10, 1e10, True)
+        score, action = self.alphabeta(cstate, self.ply, -1e10, 1e10, True, None, debug)
 
         if debug:
             if debug == 2:
                 print(action)
             print('States evalueated - {}'.format(self.states_evaluated))
             print('Time taken: {}'.format(time.time() - t1))
-        # print(action)
+        print(action)
         return action
 
-    def alphabeta(self, state, depth, alpha, beta, maximizingPlayer):
+    def alphabeta(self, state, depth, alpha, beta, maximizingPlayer, selected_move = None, debug = False):
         if env.is_done(state) or depth == 0:
-            return env.score(self.player, state)
+            if debug:
+                for i in range(3-depth):
+                    print("\t", end='')
+                print("leaf node: {}".format([env.score(self.player, state), selected_move]))
+            return [env.score(self.player, state), selected_move]
 
+        if debug:
+            for i in range(3-depth):
+                print("\t", end='')
         if maximizingPlayer:
+            if debug:
+                print('choosing max')
             value = -1e10
             actions = env.moves(self.player, state)
             for a in actions:
                 self.states_evaluated += 1
                 new_state = env.apply_move(a, state)
-                score = self.alphabeta(new_state, depth-1, alpha, beta, False)
+
+                if selected_move == None:
+                    score, _ = self.alphabeta(new_state, depth-1, alpha, beta, False, a, debug)
+                else:
+                    score, _ = self.alphabeta(new_state, depth-1, alpha, beta, False, selected_move, debug)
                 value = max(value, score)
                 alpha = max(alpha, value)
                 if alpha >= beta:
                   break
-            return value
+            if debug:
+                for i in range(3-depth):
+                    print("\t", end='')
+                print([value, selected_move])
+            return [value, selected_move]
         else:
+            if debug:
+                print('choosing min')
             value = 1e10
             actions = env.moves(self.opponent, state)
             for a in actions:
                 self.states_evaluated += 1
                 new_state = env.apply_move(a, state)
-                score = self.alphabeta(new_state, depth-1, alpha, beta True)
+                score, _ = self.alphabeta(new_state, depth-1, alpha, beta, True, selected_move, debug)
                 value = min(value, score)
                 beta = min(beta, value)
-                if alpha >= beta then
+                if alpha >= beta:
                   break
-            return value
+
+            if debug:
+                for i in range(3-depth):
+                    print("\t", end='')
+                print([value, selected_move])
+            return [value, selected_move]
